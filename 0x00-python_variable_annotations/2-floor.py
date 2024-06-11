f6fae0a93fe87bf08e6a571cfbe21f1dc02611c9
johnny <johnametepeagboku@gmail.com>
2024-06-11 13:00:34 +0000

    Update: Task 2 to 4

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module contains a floor"""
+import math
+
+
+def floor(n: float) -> float:
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""The module contians the function 'to_str()' that converts an integer to a string."""
+
+
+def to_str(number: float) -> str:
+    return str(number)
